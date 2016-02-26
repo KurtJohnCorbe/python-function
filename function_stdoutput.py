aed510d0b1862b6d8203a0ff5646f637e3c0fefd
Kurt John P. Corbe <anebroc121@yahoo.com>
2016-02-26 04:04:34 -0500

    MyFunc to FuncOut

---
+++ function_stdoutput.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python
+
+def FuncOut(k, j):
+    print "Hi!my name is ",k +"and my age is",j
+    print "\nHi!my name is {}  and my age is {}".format(k,j)
+    print "\nHi! my name is %s  and my age is %s"%(k,j)
+
+myFunc("Mary",19)
